baed60eb2a30a98ef43d22dc5d941909c6abaa6c
IoyoCode <me@ioyo.dev>
2023-09-02 21:24:17 +0100

    feat: Updated base url

--- src/main/java/net/candorservices/java/utils/Routes.java
+++ src/main/java/net/candorservices/java/utils/Routes.java
@@ -2,7 +2,7 @@ package net.candorservices.java.utils;
 
 public class Routes {
 
-    public final static String BASE_URL = "https://candorian.app/api";
+    public final static String BASE_URL = "https://dashboard.candorservices.net/api";
 
     public final static Route VERIFY_LICENSE = new Route("/licenses/verify", Route.Method.POST);
     public final static Route GET_CONFIG = new Route("/configs/:id", Route.Method.GET);
